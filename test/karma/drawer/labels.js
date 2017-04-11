1f301472f33df906c141f0fc7b2245803bf7eb51
Korney Czukowski <carbofos@seznam.cz>
2017-04-11 09:42:58 +0200

    Move filter data function to chart component, separate lines component, move data function to drops component, backwards compatibility

--- test/karma/drawer/labels.js
+++ test/karma/drawer/labels.js
@@ -1,3 +1,4 @@
+import { createDefaultComponents } from '../../../lib/config';
 import LabelsComponent from '../../../lib/drawer/labels';
 
 describe('Labels drawer', () => {
@@ -16,6 +17,7 @@ describe('Labels drawer', () => {
 
     let svg;
     let container;
+    let components;
     let object;
 
     beforeEach(() => {
@@ -27,30 +29,29 @@ describe('Labels drawer', () => {
             .append('g')
             .classed('labels', true)
             .attr('transform', 'translate(0, 45)');
+        components = createDefaultComponents({date: d => d});
         object = new LabelsComponent;
     });
 
     it('should apply labelsWidth config', () => {
         scales.x.domain(domain);
-        object.create(container, null, scales, config)(data);
+        object.create(container, null, scales, config, components)(data);
         expect(+svg.select('.label').attr('x')).toBe(50);
     });
 
     it('should show label count with simple data', () => {
         const data = [{name: 'foo', data: [new Date('2014-02-01'), new Date('2014-03-01')]}];
         scales.x.domain(domain);
-        object.create(container, null, scales, config)(data);
+        object.create(container, null, scales, config, components)(data);
         expect(document.querySelector('.label').textContent).toBe('foo (2)');
     });
 
     it('should show label count with complex data', () => {
-        const config =  {
-            date: d => d.date
-        };
         const complexData = [{ name: 'foo', data: [{ test: 'bar', date: new Date('2014-02-01') }, { test: 'baz', date: new Date('2014-03-01') }]}];
+        components.drops.dateFunction = (d) => d.date;
 
         scales.x.domain(domain);
-        object.create(container, null, scales, config)(complexData);
+        object.create(container, null, scales, config, components)(complexData);
 
         expect(document.querySelector('.label').textContent).toBe('foo (2)');
     });
